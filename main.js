a8e53d51e9119cf033a6782738f8565db4d21b71
beata <beata@emc2wave.com>
2019-10-21 14:15:56 -0700

    add getRandomLower getRandomUpper

---
+++ main.js
@@ -0,0 +1,9 @@
+// Generator functions - http://www.net-comber/charset.html
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
